9fdeddaab3baa24be698c6c7cfb48698a35a4520
Sujith Varughese <sujithv15@gmail.com>
2025-02-25 02:15:30 -0500

    react router routing fix

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import '@mantine/core/styles.css';
 import {MantineProvider} from "@mantine/core";
 import {Provider} from "react-redux";
 import {store} from "./store.ts";
-import {BrowserRouter, Route, Routes} from "react-router";
+import {HashRouter, Route, Routes} from "react-router";
 import Support from "./components/Support.tsx";
 import PrivacyPolicy from "./components/PrivacyPolicy.tsx";
 import Contact from "./components/Contact.tsx";
@@ -15,14 +15,14 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
       <MantineProvider>
-        <BrowserRouter>
+        <HashRouter>
           <Routes>
             <Route path="/" element={<App />} />
             <Route path="/support" element={<Support />} />
             <Route path="/privacy" element={<PrivacyPolicy />} />
             <Route path="/contact" element={<Contact />} />
           </Routes>
-        </BrowserRouter>
+        </HashRouter>
 
       </MantineProvider>
     </Provider>
