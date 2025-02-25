ffa49be954c797fdf4ed735af267d0bc5570b28c
Sujith Varughese <sujithv15@gmail.com>
2025-02-25 02:19:09 -0500

    developer info added

--- src/components/Support.tsx
+++ src/components/Support.tsx
@@ -7,8 +7,9 @@ const Support = () => {
       <Flex style={{ justifyContent: "center", alignItems: "center", margin: 16 }}>
         <Box>
           <Text style={{ fontSize: 56, fontWeight: 600, marginBottom: 24 }}>Trivia Night AI</Text>
-          <Text style={{ color: "gray"}}>Ultimate Trivia Challenge – Test Your Knowledge & Learn with AI!
+          <Text style={{ color: "gray", marginBottom: 24 }}>Ultimate Trivia Challenge – Test Your Knowledge & Learn with AI!
             Think you’ve got what it takes to be the ultimate trivia champion? Challenge yourself with Trivia Night AI, the exciting quiz game where you test your knowledge across multiple categories and aim for the highest score!</Text>
+          <Text style={{ color: "gray"}}>Developer Email: sujithv15@gmail.com</Text>
         </Box>
 
         <Box>
